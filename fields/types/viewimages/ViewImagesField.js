61db4186ae1c15d1393d3e2edbb20069097536af
5202m <648224806@qq.com>
2016-01-28 10:28:25 +0800

    Create ViewImagesField.js

---
+++ fields/types/viewimages/ViewImagesField.js
@@ -0,0 +1,89 @@
+var _ = require('underscore'),
+	React = require('react'),
+	Field = require('../Field');
+
+var Thumbnail = React.createClass({
+	
+	displayName: 'ViewImagesField',
+	
+	render: function() {
+		var iconClassName, imageDetails;
+
+		var previewClassName = 'image-preview';
+		
+		var title = '';
+		var width = this.props.width;
+		var height = this.props.height;
+		var styles = {
+			height: '90'
+		};
+		if (width && height) {
+			title = width + ' x ' + height;
+			styles = {
+				width:  width,
+				height: height
+			};
+		}
+		var actionLabel = this.props.deleted ? 'Undo' : 'Remove';
+
+		var imgUrl = this.props.url + this.props._id;
+		return (
+			<div className='image-field image-sortable row col-sm-3 col-md-12' title={title}> 
+				<div className={previewClassName}> 
+					<a href={imgUrl} className='img-thumbnail'>
+						<img style={styles} className='img-load' src={imgUrl} />
+						<span className={iconClassName} />
+					</a>
+				</div>
+
+				
+			</div>
+		);
+	}
+	
+});
+
+module.exports = Field.create({
+
+	getInitialState: function() {
+		var thumbnails = [];
+		var self = this;
+		var url = this.props.paths.src;
+		var width = this.props.paths.width;
+		var height = this.props.paths.height;
+		_.each(this.props.value, function (item) {
+			item.url = url;
+			item.width = width;
+			item.height = height;
+			self.pushThumbnail(item, thumbnails);
+		});
+
+		return { thumbnails: thumbnails };
+	},
+
+	pushThumbnail: function (args, thumbs) {
+		thumbs = thumbs || this.state.thumbnails;
+		var i = thumbs.length;
+		thumbs.push(<Thumbnail key={i} {...args} />);
+	},
+
+	renderContainer: function() {
+		return (
+			<div className='images-container clearfix'>
+				{this.state.thumbnails}
+			</div>
+		);
+	},
+	
+	renderUI: function() {
+		return (
+			<div className='field field-type-cloudinaryimages'>
+				<label className='field-label'>{this.props.label}</label>
+
+				<div className='field-ui'>
+					{this.renderContainer()}
+				</div>
+			</div>
+		);
+	}
+});
